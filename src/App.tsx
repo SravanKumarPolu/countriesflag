dfb52b1ab90d86af7329984b037e17314244d30c
PVSKR <skr.polu@hotmail.com>
2024-03-26 21:29:53 +0530

    in this case i used useEffect, useState, createContext, useContext hooks of react.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
-import CountriesFlagList from "./components/CountriesFlagList";
-import SearchBar from "./components/SearchBar";
+import { useEffect, useState, createContext, useContext } from "react";
 
-import useCountryFlagsSource from "./components/useCountryFlagsSource";
-import { CountriesProvider } from "./hooks/useCountryFlags";
+interface CountryFlag {
+  callingCodes: number;
+  name: string;
+  capital: string;
+  region: string;
+  population: number;
+  flag: string;
+  code: string;
+  independent: boolean;
+}
+function useCountrFlag(): {
+  countryFlag: CountryFlag[];
+} {
+  const [countryFlag, setCountryFlag] = useState<CountryFlag[]>([]);
+
+  useEffect(() => {
+    fetch(`/countries.json`)
+      .then((response) => response.json())
+
+      .then((data) => setCountryFlag(data));
+  }, []);
+  return { countryFlag };
+}
+
+const CountryFlagContext = createContext({ countryFlag: [] as CountryFlag[] });
+
+const CountryFlagList = () => {
+  const { countryFlag } = useContext(CountryFlagContext);
+  return (
+    <div>
+      {countryFlag.map((c) => (
+        <div key={c.callingCodes}>{c.name}</div>
+      ))}
+    </div>
+  );
+};
 
 function App() {
-  const { country } = useCountryFlagsSource();
   return (
     <>
-      <CountriesProvider>
+      <CountryFlagContext.Provider value={useCountrFlag()}>
         <div className="container mx-auto p-4 max-w-3xl">
           <h1 className="text-[40px] font-bold mb-4 text-center">
             Countryflags.com
           </h1>
-          <SearchBar />
 
-          <CountriesFlagList countries={country} />
+          <CountryFlagList />
         </div>
-      </CountriesProvider>
+      </CountryFlagContext.Provider>
     </>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,16 +1,16 @@
-import useCountryFlagsSource from "./useCountryFlagsSource";
+// import useCountryFlagsSource from "./useCountryFlagsSource";
 
-const SearchBar = () => {
-  const { search, setSearch } = useCountryFlagsSource();
-  return (
-    <input
-      className="mt-3 pl-2 block w-full  rounded  text-blue-400
-      border border-gray-300 shadow-md"
-      placeholder="search..."
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-    />
-  );
-};
+// const SearchBar = () => {
+//   const { search, setSearch } = useCountryFlagsSource();
+//   return (
+//     <input
+//       className="mt-3 pl-2 block w-full  rounded  text-blue-400
+//       border border-gray-300 shadow-md"
+//       placeholder="search..."
+//       value={search}
+//       onChange={(e) => setSearch(e.target.value)}
+//     />
+//   );
+// };
 
-export default SearchBar;
+// export default SearchBar;
